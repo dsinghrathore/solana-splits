1cf0412c89ffdf1561e0030526f0be21c34993c8
anoushk1234 <anoushka1828@gmail.com>
2022-01-08 21:09:21 +0530

    btree map

--- programs/split/src/lib.rs
+++ programs/split/src/lib.rs
@@ -1,6 +1,7 @@
 use anchor_lang::prelude::Pubkey;
 use anchor_lang::prelude::*;
 use percentage::Percentage;
+use std::collections::BTreeMap;
 use std::collections::HashMap;
 
 declare_id!("4tzDAD5KLntPhT8t3gjqs85vsT5aguZTNCoeRvKkt5zr");
@@ -15,13 +16,13 @@ pub mod split {
         Ok(())
     }
 
-    pub fn new_split(ctx: Context<NewSplitContext>, split: Vec<Map>) -> ProgramResult {
+    pub fn new_split(ctx: Context<NewSplitContext>, split: BTreeMap<Pubkey, u64>) -> ProgramResult {
         let base_account = &mut ctx.accounts.base_account;
         let mut total_percentage = 0;
         let mut index = 0;
 
         for item in split.iter() {
-            total_percentage = total_percentage + item.perc;
+            total_percentage = total_percentage + item.1;
             index = index + 1;
         }
 
@@ -47,24 +48,25 @@ pub mod split {
         let mut index = 0;
 
         for rc_account in ctx.remaining_accounts.iter() {
-            for split_map in split_ref.iter() {
-                if &split_map.pub_key == &rc_account.key() {
-                    let split_percentage = Percentage::from(split_map.perc);
-                    let split_amount = split_percentage.apply_to(amount);
-                    let ix = anchor_lang::solana_program::system_instruction::transfer(
-                        &msg_sender.key(),
-                        &rc_account.key(),
-                        split_amount,
-                    );
-                    anchor_lang::solana_program::program::invoke(
-                        &ix,
-                        &[
-                            msg_sender.to_account_info(),
-                            ctx.remaining_accounts[index].to_account_info(),
-                        ],
-                    );
-                    index = index + 1;
-                }
+            if split_ref.contains_key(&rc_account.key()) {
+                let split_percentage = Percentage::from(split_ref[&rc_account.key()]);
+                let split_amount = split_percentage.apply_to(amount);
+
+                let ix = anchor_lang::solana_program::system_instruction::transfer(
+                    &msg_sender.key(),
+                    &rc_account.key(),
+                    split_amount,
+                );
+
+                anchor_lang::solana_program::program::invoke(
+                    &ix,
+                    &[
+                        msg_sender.to_account_info(),
+                        ctx.remaining_accounts[index].to_account_info(),
+                    ],
+                );
+
+                index = index + 1;
             }
 
             panic!("account address doesn't exist in splits info");
@@ -74,17 +76,17 @@ pub mod split {
     }
 }
 
-#[state]
-#[derive(Default, Copy)]
-pub struct Map {
-    pub pub_key: Pubkey,
-    pub perc: u64,
-}
+// #[state]
+// #[derive(Default, Copy)]
+// pub struct Map {
+//     pub pub_key: Pubkey,
+//     pub perc: u64,
+// }
 
 #[account]
 pub struct BaseAccount {
     pub splits_count: u64,
-    pub splits: HashMap<u64, Vec<Map>>,
+    pub splits: BTreeMap<u64, BTreeMap<Pubkey, u64>>,
 }
 
 #[derive(Accounts)]
