38cc7560f0fd4a4431e65fb5a6a5d47598e0375a
Digvijay Singh Rathore <digvijaysra@gmail.com>
2022-01-13 19:45:25 +0530

    removed commented code

--- programs/split/src/lib.rs
+++ programs/split/src/lib.rs
@@ -25,7 +25,6 @@ pub mod split {
         ctx: Context<NewSplitContext>,
         split_perc: Vec<u64>,
         split_keys: Vec<Pubkey>,
-        // nonce: u8
     ) -> ProgramResult {
         let base_account = &mut ctx.accounts.base_account;
         let mut total_percentage = 0;
@@ -41,10 +40,6 @@ pub mod split {
             "NEW SPLIT: total percentage should be 100"
         );
 
-        // CREATING A PDA
-
-        // base_account.splits_perc.push(split_perc);
-        // base_account.splits_keys.push(split_keys);
         let n_split = Split {
             splits_creator: ctx.accounts.user.key(),
             splits_percentage: split_perc,
@@ -61,24 +56,11 @@ pub mod split {
     pub fn send_sol<'a, 'b, 'c, 'info>(
         ctx: Context<'a, 'b, 'c, 'info, SenderContext<'info>>,
         split_id: u64,
-        amount: u64,
-        // nonce: u8
-                   // receivers: Vec<Account<'info, T>>
+        amount: u64
     ) -> ProgramResult {
-        // let split_perc = &ctx.accounts.base_account.splits_perc[split_id as usize];
-        // let split_keys = &ctx.accounts.base_account.splits_keys[split_id as usize];
         let current_split = &mut ctx.accounts.base_account.splits[split_id as usize];
-        // let split_perc = &current_split.splits_percentage;
-        // let split_keys = &current_split.splits_keys;
         let msg_sender = &mut ctx.accounts.user;
-        // let mut index = 0;
 
-        // let bank_pda = Pubkey::create_program_address(
-        //     &[msg_sender.to_account_info().key.as_ref(), &[nonce]],
-        //     ctx.program_id,
-        // );
-        // let bank_res = bank_pda.unwrap_or_default();
-        // TRANSFER MONEY TO PDA AND STORE IT IN STRUCT
         let n_payment = Payment {
             total_amount: amount,
             paid_to: vec![],
@@ -99,32 +81,6 @@ pub mod split {
         )?;
 
         current_split.payments.push(n_payment);
-
-        // for rc_account in ctx.remaining_accounts.iter() {
-        // for rc_account in receivers.iter() {
-        //     if split_keys.contains(&rc_account.key()) {
-        //         if rc_account.key() != msg_sender.key() {
-        //             let split_percentage = Percentage::from(split_perc[index]);
-        //             let split_amount = split_percentage.apply_to(amount);
-
-        //             let ix = anchor_lang::solana_program::system_instruction::transfer(
-        //                 &msg_sender.key(),
-        //                 &rc_account.key(),
-        //                 split_amount,
-        //             );
-
-        //             anchor_lang::solana_program::program::invoke(
-        //                 &ix,
-        //                 &[
-        //                     msg_sender.to_account_info(),
-        //                     receivers[index].to_account_info(),
-        //                 ],
-        //             );
-        //         }
-
-        //         index = index + 1;
-        //     }
-        // }
 
         Ok(())
     }
@@ -172,32 +128,7 @@ pub mod split {
 
         Ok(())
     }
-
-    // fn calculate_hash<T: Hash>(t: &T) -> u64 {
-    //     let mut s = DefaultHasher::new();
-    //     t.hash(&mut s);
-    //     s.finish()
-    // }
-
-    // pub fn create_with_seed(
-    //     base: &Pubkey,
-    //     seed: &str,
-    //     program_id: &Pubkey,
-    // ) -> Result<Pubkey> {
-    //     if seed.len() > 60 {
-    //         return Err(SystemError::MaxSeedLengthExceeded);
-    //     }
-    //     Ok(Pubkey::new(
-    //         hashv(&[base.as_ref(), seed.as_ref(), program_id.as_ref()]).as_ref(),
-    //     ))
-    // }
 }
-
-// TODO:
-// 1. Create PDA
-// 2. Send SOL to it
-// 3. Store it in the struct
-// 4. Transfer SOL from it
 
 #[derive(Debug, Clone, AnchorSerialize, AnchorDeserialize)]
 pub struct Split {
@@ -217,11 +148,6 @@ pub struct Payment {
 pub struct BaseAccount {
     pub splits: Vec<Split>,
 }
-
-// #[account]
-// pub struct BankAccount {
-//     pub balance: u64,
-// }
 
 #[derive(Accounts)]
 pub struct Initialize<'info> {
